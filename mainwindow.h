22540611cb03ad029c07addb9be12afe786696f2
Tharindu Lakmal Yapa <51540855+yapa-ymtl@users.noreply.github.com>
2025-07-14 22:03:01 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -24,3 +24,31 @@ private:
     MyGLWidget* glWidget;
 };
 #endif // MAINWINDOW_H
+----------
+    #pragma once
+
+#include <QOpenGLWidget>
+#include <AIS_InteractiveContext.hxx>
+#include <V3d_View.hxx>
+#include <OpenGl_GraphicDriver.hxx>
+#include <TopoDS_Shape.hxx>
+
+class MyGLWidget : public QOpenGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyGLWidget(QWidget* parent = nullptr);
+    ~MyGLWidget();
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void showEvent(QShowEvent* event) override;
+
+private:
+    Handle(OpenGl_GraphicDriver) m_driver;
+    Handle(AIS_InteractiveContext) m_context;
+    Handle(V3d_View) m_view;
+    TopoDS_Shape m_shape;
+};
